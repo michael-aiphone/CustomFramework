15b8e933ac133d2996df2d000463ba0ab7823d84
Michael <michael.sweeney@aiphone.com>
2023-11-17 12:33:10 -0800

    initial commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by Michael Sweeney on 11/17/23.
+//
+
+import Foundation
+
+public struct SDK {
+    static func doSomeWork() {
+        print("Doing some work ...")
+    }
+}
